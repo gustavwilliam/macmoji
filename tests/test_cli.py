df1929c69dab9a944768fa002d3aa2a365c5346a
Gustav Odinger <65498475+gustavwilliam@users.noreply.github.com>
2023-07-01 22:06:13 +0200

    Add basic tests

---
+++ tests/test_cli.py
@@ -0,0 +1,14 @@
+import pytest
+import importlib.metadata
+from typer.testing import CliRunner
+
+from macmoji.__main__ import app
+
+runner = CliRunner()
+
+
+@pytest.mark.parametrize("arg", ["--version", "-V"])
+def test_version(arg):
+    result = runner.invoke(app, arg)
+    assert result.exit_code == 0
+    assert f"MacMoji {importlib.metadata.version('macmoji')}" in result.stdout
